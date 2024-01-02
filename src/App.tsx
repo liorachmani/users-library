81fe7d88eb4e87ad3c8d7587fa6cd52c60b41ab1
Lior Rachmani <liorachmani@gmail.com>
2024-01-02 10:22:53 +0200

    fetching data and update store via react custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,6 @@
-import React, { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import {
-  ApiUser,
-  fetchUsersSuccess,
-  selectFilteredUsers,
-} from "../src/store/usersSlice";
-import axios, { AxiosError } from "axios";
-import { API_URL } from "./constants";
+import React from "react";
+import { useSelector } from "react-redux";
+import { selectFilteredUsers } from "../src/store/usersSlice";
 import {
   UserCardsList,
   NewUserButton,
@@ -15,52 +9,11 @@ import {
   SearchBar,
 } from "./components";
 import styles from "./App.module.css";
-import { Row } from "antd";
-import { extractUserInfo } from "./utils/extractUserInfo";
-
-type ApiInfo = {
-  info: {
-    page: number;
-    results: number;
-    seed: string;
-    version: string;
-  };
-  results: ApiUser[];
-};
+import { useFetchUsers } from "./hooks/useFetchUsers";
 
 const App: React.FC = () => {
-  const [loading, setLoading] = useState<boolean>(false);
-  const [error, setError] = useState<string>("");
-
-  const dispatch = useDispatch();
+  const { loading, error } = useFetchUsers();
   const filteredUsers = useSelector(selectFilteredUsers);
-
-  useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        setLoading(true);
-        const { data }: { data: ApiInfo } = await axios.get(API_URL);
-
-        const usersArr = data.results.map(extractUserInfo);
-
-        setLoading(false);
-
-        // Update the Redux store with the fetched data
-        dispatch(fetchUsersSuccess(usersArr));
-      } catch (error: any | AxiosError) {
-        if (axios.isAxiosError(error)) {
-          const { message, status } = error;
-          setError(`Error => status code ${status} - message: ${message}`);
-        } else {
-          setError(`Error => ${error}`);
-        }
-        setLoading(false);
-      }
-    };
-
-    // Call the fetchUsers function when the component mounts
-    fetchUsers();
-  }, [dispatch]);
 
   return (
     <>
@@ -68,9 +21,7 @@ const App: React.FC = () => {
 
       <SearchBar />
 
-      <Row justify={"center"}>
-        <NewUserButton />
-      </Row>
+      <NewUserButton />
 
       {loading && <Loading />}
 
